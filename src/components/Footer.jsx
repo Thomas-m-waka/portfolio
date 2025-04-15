6d0d7101269e7357cf793845bc7639c6290d347d
Thomas-m-waka <thomasmmuteti4@gmail.com>
2025-04-15 12:24:54 +0300

    update images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,31 @@
+import React from 'react';
+import { FaGithub, FaLinkedin, FaInstagram, FaXTwitter } from 'react-icons/fa6'; // 'FaXTwitter' is from react-icons/fa6
+import './Footer.css';
+
 const Footer = () => {
-    return (
-      <footer className="footer">
+  return (
+    <footer className="footer">
+      <div className="footer-content">
         <div className="social-icons">
-          <a href="https://github.com/Thomas-m-waka" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="https://www.linkedin.com/in/thomas-muteti-859542285/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://github.com/Thomas-m-waka" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="icon" /> GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/thomas-muteti-859542285/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="icon" /> LinkedIn
+          </a>
+          <a href="https://www.instagram.com/developer_tom/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="icon" /> Instagram
+          </a>
+          <a href="https://x.com/DevTom_3" target="_blank" rel="noopener noreferrer">
+            <FaXTwitter className="icon" /> X 
+          </a>
         </div>
-        <p className="ph">© 2024. All rights reserved.</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
+        <p className="ph">© 2025. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './Home.css';
-import tomImage from '../assets/tom.jpg';  
+import tomImage from '../assets/tom.jpg'; 
+import { Link } from 'react-router-dom';
+
 
 const Home = () => (
   <section className="hero-section">
     <div className="text-container">
       <h1>Hi, I'm a Software Developer</h1>
-      <p>Crafting solutions with code and creativity. Passionate about building innovative and efficient web applications.</p>
-      <a href="/contact" className="cta">Contact Me</a>
+      <p>
+        I am a highly efficient Software Developer with a comprehensive skill set spanning both
+        frontend and backend development, including database management. I possess expertise in
+        <strong> Django, Python, HTML5, CSS, JavaScript, and React</strong>, with a strong focus on creating scalable
+        and secure applications. Recognized for my team-oriented approach, I bring excellent
+        interpersonal and communication skills to collaborate effectively on projects.
+      </p>
+
+      <Link to="/contact" className="cta">Contact Me</Link>
     </div>
     <div className="image-container">
       <img className="profile-image" src={tomImage} alt="Tom" />
